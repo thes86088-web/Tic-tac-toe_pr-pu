a5e5365471e66857f5ae03da6631de18f0d60882
durvesh <thes86088@gmail.com>
2026-02-19 11:22:36 +0530

    create skeleton for abstractTwo.js

---
+++ abstractTwo.js
@@ -0,0 +1,61 @@
+function initMat()
+{
+  
+}
+
+function populate()
+{
+  
+}
+
+function getPosition()
+{
+  
+}
+
+function placeOnGrid()
+{
+  
+}
+
+function changeStyle()
+{
+  
+}
+
+function checkGrid()
+{
+  
+}
+
+function fetchBest()
+{
+  
+}
+
+function checkRows() 
+{
+  
+}
+
+function checkColumns() 
+{
+    
+}
+
+function checkDiag() 
+{
+  
+}
+
+function checkCrossDiag() 
+{
+  
+}
+
+function blinkElemetsOf() 
+{
+  
+}
+
+
